1dcfa052aa567bbc925ee756aece580bf721a966
seike460 <seike460@gmail.com>
2018-11-14 05:06:34 +0900

    [ci skip]:+1: Hardware::CPU.is_64_bit

--- s3ry.rb
+++ s3ry.rb
@@ -1,7 +1,7 @@
 class S3ry < Formula
   desc "Amazon S3 CLI Tool by using promptui"
   homepage "https://godoc.org/github.com/seike460/s3ry"
-  if Hardware.is_64_bit?
+  if Hardware::CPU.is_64_bit?
     url "https://github.com/seike460/s3ry/releases/download/0.1.1/s3ry_darwin_amd64.zip"
     sha256 "b9f8eab7950e9687f8b2922a9580a78420e1b3e0888e49178e69a8ce1ade0744"
   else

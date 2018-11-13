c7b6a3ec02fecb53cf3ac77b0ac4e6fc16946d54
seike460 <seike460@gmail.com>
2018-11-14 03:12:30 +0900

    [WIP] [ci skip]s 最新リリースに合わせる

--- s3ry.rb
+++ s3ry.rb
@@ -1,9 +1,9 @@
 class S3ry < Formula
   desc "Amazon S3 CLI Tool by using promptui"
   homepage "https://godoc.org/github.com/seike460/s3ry"
-  url "https://github.com/seike460/s3ry/releases/download/0.1/s3ry_darwin_amd64.zip"
-  sha256 "bcfe4b6bfde5bde4707781a41aaf6c7a498336fc0462f1bb7f76b84f2f6dc54f"
-  version "0.2.0"
+  url "https://github.com/seike460/s3ry/releases/download/0.1.1/s3ry_darwin_amd64.zip"
+  sha256 "b9f8eab7950e9687f8b2922a9580a78420e1b3e0888e49178e69a8ce1ade0744"
+  version "0.1.1"
   def install
     bin.install 's3ry'
   end

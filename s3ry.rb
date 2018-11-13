51328f41f0fa6379f80029f6084e5dfc8a7cc8f4
seike460 <seike460@gmail.com>
2018-11-14 02:56:21 +0900

    [WIP] [ci skip] インストールするだけにする

--- s3ry.rb
+++ s3ry.rb
@@ -5,7 +5,6 @@ class S3ry < Formula
   sha256 "bcfe4b6bfde5bde4707781a41aaf6c7a498336fc0462f1bb7f76b84f2f6dc54f"
   version "0.2.0"
   def install
-    system "go", "build", "-o", "s3ry", "cmd/main.go"
     bin.install 's3ry'
   end
   test do

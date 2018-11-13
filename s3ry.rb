dc9682fa01d8b0855b0948043378507e7790dbc4
seike460 <seike460@gmail.com>
2018-11-14 04:11:09 +0900

    [WIP] [ci skip] 386対応

--- s3ry.rb
+++ s3ry.rb
@@ -1,8 +1,13 @@
 class S3ry < Formula
   desc "Amazon S3 CLI Tool by using promptui"
   homepage "https://godoc.org/github.com/seike460/s3ry"
-  url "https://github.com/seike460/s3ry/releases/download/0.1.1/s3ry_darwin_amd64.zip"
-  sha256 "b9f8eab7950e9687f8b2922a9580a78420e1b3e0888e49178e69a8ce1ade0744"
+  if Hardware.is_64_bit?
+    url "https://github.com/seike460/s3ry/releases/download/0.1.1/s3ry_darwin_amd64.zip"
+    sha256 "b9f8eab7950e9687f8b2922a9580a78420e1b3e0888e49178e69a8ce1ade0744"
+  else
+    url "https://github.com/seike460/s3ry/releases/download/0.1.1/s3ry_darwin_386.zip"
+    sha256 "135b5cdda3ae30d60efd6afa9f49d0d288258a990b59d10aca07f3ea4b68c7ea"
+  end
   version "0.1.1"
   def install
     bin.install 's3ry'

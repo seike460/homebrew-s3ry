b7a6ef94673d2396dce8ab8c4f319ab757e7e2d3
seike460 <seike460@gmail.com>
2018-11-14 02:45:52 +0900

    [WIP] [ci skip] Formula

---
+++ s3ry.rb
@@ -0,0 +1,13 @@
+class S3ry < Formula
+  desc "Amazon S3 CLI Tool by using promptui"
+  homepage "https://godoc.org/github.com/seike460/s3ry"
+  url "https://github.com/seike460/s3ry/releases/download/0.1/s3ry_darwin_amd64.zip"
+  sha256 "bcfe4b6bfde5bde4707781a41aaf6c7a498336fc0462f1bb7f76b84f2f6dc54f"
+  version 0.2
+  def install
+    system "go", "build", "-o", "s3ry", "cmd/main.go"
+  end
+  test do
+    system "true"
+  end
+end

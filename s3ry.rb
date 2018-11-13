bb9206974c61af2cd183f0972347701b8064cb96
seike460 <seike460@gmail.com>
2018-11-14 02:47:18 +0900

    [WIP] [ci skip] 0.2.0

--- s3ry.rb
+++ s3ry.rb
@@ -3,7 +3,7 @@ class S3ry < Formula
   homepage "https://godoc.org/github.com/seike460/s3ry"
   url "https://github.com/seike460/s3ry/releases/download/0.1/s3ry_darwin_amd64.zip"
   sha256 "bcfe4b6bfde5bde4707781a41aaf6c7a498336fc0462f1bb7f76b84f2f6dc54f"
-  version 0.2
+  version "0.2.0"
   def install
     system "go", "build", "-o", "s3ry", "cmd/main.go"
   end

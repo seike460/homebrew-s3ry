e09c51b94118b735025f68b245380c1bf1dc8d22
seike460 <seike460@gmail.com>
2018-11-24 00:45:13 +0900

    [ci skip]:+1:

--- s3ry.rb
+++ s3ry.rb
@@ -3,10 +3,10 @@ class S3ry < Formula
   homepage "https://godoc.org/github.com/seike460/s3ry"
   if Hardware::CPU.is_64_bit?
     url "https://github.com/seike460/s3ry/releases/download/0.1.2/s3ry_darwin_amd64.zip"
-    sha256 "ef3222c87f70043d1ce668d9e56f86b29909491182c8cc24cdda2bce278a9b4a"
+    sha256 "3f290d732f06cb6d0f7e29bbd62b1cb7594efe6bd47d8a156ddc7915f0e6d1e7"
   else
     url "https://github.com/seike460/s3ry/releases/download/0.1.2/s3ry_darwin_386.zip"
-    sha256 "774c88622873e043b0660aa1ae9e1a853104a5dadc984072bbe0bcb664e66ac8"
+    sha256 "f3123b256aec5d1c375b0d1f1da80f824d471131ad581e6e0a00a5645eb934a4"
   end
   version "0.1.2"
   def install

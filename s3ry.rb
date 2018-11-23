ad3ca7a617e08be4f2962181371855d617205e10
seike460 <seike460@gmail.com>
2018-11-24 00:09:00 +0900

    [ci skip]:+1: 0.1.2

--- s3ry.rb
+++ s3ry.rb
@@ -2,13 +2,13 @@ class S3ry < Formula
   desc "Amazon S3 CLI Tool by using promptui"
   homepage "https://godoc.org/github.com/seike460/s3ry"
   if Hardware::CPU.is_64_bit?
-    url "https://github.com/seike460/s3ry/releases/download/0.1.1/s3ry_darwin_amd64.zip"
-    sha256 "b9f8eab7950e9687f8b2922a9580a78420e1b3e0888e49178e69a8ce1ade0744"
+    url "https://github.com/seike460/s3ry/releases/download/0.1.2/s3ry_darwin_amd64.zip"
+    sha256 "906fd8aa36bce70fc4e117f740eb83e9c063f71465e97ad0ca178d952f4c5904"
   else
-    url "https://github.com/seike460/s3ry/releases/download/0.1.1/s3ry_darwin_386.zip"
-    sha256 "135b5cdda3ae30d60efd6afa9f49d0d288258a990b59d10aca07f3ea4b68c7ea"
+    url "https://github.com/seike460/s3ry/releases/download/0.1.2/s3ry_darwin_386.zip"
+    sha256 "d6ae99f1664626b52986a0b3ac0003cf7773c2732f51783299be0f3ae238d47b"
   end
-  version "0.1.1"
+  version "0.1.2"
   def install
     bin.install 's3ry'
   end
